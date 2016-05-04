0e47cb1a8eac1a0a45c5000dde68db1747c5e5b1
Ellis Brown <elbrown47@gmail.com>
2016-05-04 08:18:33 -0500

    run_search.py

--- server/run_search.py
+++ server/run_search.py
@@ -1,8 +1,21 @@
 #from subprocess import call
 import sys
-sys.path.append('../pagerank/')
-pagerank = __import__('6_search.search')
+import wikipedia
+import json
+sys.path.append('../makeHBase/')
+pagerank = __import__('6_search')
+
+def get_wiki_link(title):
+    page = wikipedia.page(title)
+    summary = wikipedia.summary(page.title, sentences=1)
+    return page.title, page.url. summary
+
 
 def search(searchstring):
+    results = pagerank.search(searchstring)
+    titles = [e[0] for e in results]
+    return [get_wiki_link(title) for title in titles]
 
-    return pagerank(searchstring)
+if __name__ == '__main__':
+    keywords = sys.argv[1:]
+    print search(keywords)
